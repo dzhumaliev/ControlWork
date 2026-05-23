cd1fc516f889c85662cb2e5839c49f95a71c93e0
isakd <isak.djumaliev65@gmail.com>
2026-05-22 23:45:54 -0700

    polymorphism

--- oop.py
+++ oop.py
@@ -47,3 +47,30 @@ cat = Cat("Kitty")
 
 print(dog.name, dog.speak())  # Вывод: Buddy Woof
 print(cat.name, cat.speak())  # Вывод: Kitty Meow
+
+
+# 3
+
+class Vehicle():
+
+    def move(self):
+        return "Vehicle is moving"
+    
+class Car(Vehicle):
+    def move(self):
+        return "Car is driving"
+    
+class Bicycle(Vehicle):
+    def move(self):
+        return "Bicycle is pedaling"
+
+
+def move(vehicle):
+    return vehicle.move()
+
+
+car = Car()
+bike = Bicycle()
+
+print(move(car))  # Вывод: Car is driving
+print(move(bike))  # Вывод: Bicycle is pedaling
