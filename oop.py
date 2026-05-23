ea024e85f69329b69d8a209300df9e8b3c2137d0
isakd <isak.djumaliev65@gmail.com>
2026-05-22 23:36:45 -0700

    inheritance

--- oop.py
+++ oop.py
@@ -25,3 +25,25 @@ p.set_age(-5)  # Должна быть ошибка или предупрежд�
 
 # 2
 
+class Animal():
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        return "I am an animal"
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof"
+
+
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow"
+    
+dog = Dog("Buddy")
+cat = Cat("Kitty")
+
+print(dog.name, dog.speak())  # Вывод: Buddy Woof
+print(cat.name, cat.speak())  # Вывод: Kitty Meow
